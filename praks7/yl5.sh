7e98cd01e62eb38c75330bf485f1f95d5a74c04d
Karl Uiga <karl.uiga@voco.ee>
2026-09-18 10:57:53 +0300

    palju punkte

--- praks7/yl5.sh
+++ praks7/yl5.sh
@@ -5,11 +5,11 @@ read rida
 
 for (( i = 1; i <= rida; i++ )); do
     echo -n "$i. "
-    # Trükime esmalt (rida - i) tükki 'o' sümboleid
+
     for (( j = 1; j <= rida - i; j++ )); do
         echo -n "o "
     done
-    # Trükime seejärel (i) tükki tärne
+
     for (( k = 1; k <= i; k++ )); do
         echo -n "* "
     done
